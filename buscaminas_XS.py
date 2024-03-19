5de26b06f1831c8924e9462bb54bb514a78c7828
Soteras <156694267+Sote19@users.noreply.github.com>
2024-03-19 19:46:58 +0100

    Add files via upload

--- buscaminas_XS.py
+++ buscaminas_XS.py
@@ -26,12 +26,12 @@ class Principal:
     #-------------------------------menú dificultad-------------------------------
     
     menuDIFICULTAD = {"VS 1 Terrorista": (8, 8, 10), "VS Grupo Terrorista": (10, 10, 20), "VS Terroristas Internacionales": (12, 12, 30)}    #asignamos una variable con los valores que tendrá cada dificultad
-                                                                                              #{"Dificultad": (celdas, columnas, minas) , ...}
+    #{"Dificultad": (celdas, columnas, minas) , ...}
     
     
     def __init__(self, master):                #definimos "__init__", el cual se llamara cada vez que creemos objetos nuevos en la clase Principal
         self.master = master                   #guardamos variable master cuando se inicie la clase Principal
-        self.buttons = []                      #lista vacia para almacenar los botones del menú
+        self.botones = []                      #lista vacia para almacenar los botones del menú
 
         self.create_menu()                     #aplicamos a la función que definirá la dificultad que el usuario vaya a escoger
 
@@ -40,22 +40,59 @@ class Principal:
         self.master.config(menu=menu)
 
         difficulty_menu = tk.Menu(menu, tearoff=0)
-        menu.add_cascade(label="Dificultad", menu=difficulty_menu)
+        menu.add_cascade(label="¿A que misión quieres enfrentarte?", menu=difficulty_menu)
             
         for difficulty in Principal.menuDIFICULTAD:
             difficulty_menu.add_command(label=difficulty, command=lambda d=difficulty: self.empieza_juego(*Principal.menuDIFICULTAD[d]))
 
 
 
-    def empieza_juego (self, filas, colum, minas):
+    def empieza_juego (self, filas, colum, minas): #Creamos funcion para definir los parametros del tablero
         self.filas = filas
         self.colum = colum
-        self.minas = minas
+        self.minas = minas                                    
         self.minas_coor = []
         self.revel = set()
         self.marca = set()
         self.campo = set()
+        
+        self.crear_tabla()
+#------------------------------- Crear Tablero -----------------------------
 
+    def crear_tabla(self):
+        for i in self.botones:
+            for boton in i:
+                boton.grid_forget() #Para limpiar el tablero despues de cambiar la dificultad
+        self.botones = []
+        self.tabla = [[0] * self.colum for x in range(self.filas)]
+        self.añadir_mina()
+        self.añadir_nums()
+        
+        for y in range (self.filas):
+            fila = []
+            for j in range(self.colum):
+                boton = tk.Button(self.master, width=2, height=1, command=lambda fila=y, col=j: self.on_click(fila, col))
+                boton.bind("<Button-3>", lambda event, fila=y, col=j: self.banderin(fila, col))
+                boton.grid(fila=y, col=j)
+                fila.append(boton)
+            self.botones.append(fila)
+    
+    def añadir_mina(self):
+        self.minas_coor=random.sample([(f, c) for f in range (self.filas) for c in range (self.colum)], self.minas)
+        for fila, col in self.minas_coor:
+            self.tabla[fila][col] = -1
+            
+    def añadir_nums(self):
+        for fila, col, in self.minas_coor:
+            for f in range (fila -1, fila +2):
+                for c in range (col -1, col +2):
+                    if 0 <= f < self.filas and 0 <= c < self.colum and self.tabla[f][c] != -1: 
+                        self.tabla[f][c] += 1       
+            
+            
+            
+            
+            
 principal = Principal(root) #Creamos punto de guardado y llamamos
 
 #-------------------------------iniciar juego-------------------------------
